440b3c4e63272f1ee7dc390bc9d6a4af3c3113d6
kvaldez <kmvaldez2@gmail.com>
2016-06-08 10:07:26 -0400

    Add files via upload

--- server.R
+++ server.R
@@ -1,12 +1,16 @@
 library(shiny)
 
 function(input, output){
+  
+  #creates plot for 'how are you doing' question
   output$well = renderPlot({title = 'Here\'s a plot of random numbers based on the number you entered'
     plot(rnorm(input$well), main = title)})
   
+  #creates histogram for 'how much do you like cheese' question
   output$cheese = renderPlot({title = 'Here\'s a histogram of random numbers based on how much you like cheese'
     hist(rnorm(input$cheese), main = title)})  
   
+  #displays answer selected for 'favorite color' question
   output$colors = renderText(paste('Your favorite color is: ', input$colors))
 }
   
--- ui.R
+++ ui.R
@@ -6,23 +6,28 @@ fluidPage(titlePanel('Welcome to Kristin\'s App'),
                        h2("This writing should become increasingly smaller"),
                        h3("This should be the smallest")),
           
-          
+          #adds a slider, and creates a plot of random numbers.
+          #the amount of random numbers is determined by the number entered.
           mainPanel(sliderInput(inputId = 'well', 
             label = 'On a scale of 1 to 30, how are you doing today?',
             value = 25, min = 1, max = 30),
           
           plotOutput('well'),
           
+          #allows user to select from a drop down menu
           selectInput(inputId = 'time', 
             label = 'What time did you wake up today?', 
             choices = c('Before 8', '0800', '0900', '1000', 'Afternoon')),
           
+          #creates slider, resulting plot is a histogram of random numbers
+          #the amount of random numbers is determined by the number entered
           sliderInput(inputId = 'cheese', 
             label = 'On a scale of 1 to 30, how much do you like cheese?',
             value = 25, min = 1, max = 30),
           
           plotOutput('cheese'),
           
+          #creates radio buttons, then displays answer below
           radioButtons(inputId = 'colors',
             label = 'What is your favorite color?',
             choices = c('Blue', 'Yellow', 'Red', 'Orange', 'Green')),
